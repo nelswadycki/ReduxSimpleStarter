e94bcfd565495f441cf6043393fe85af9e284d02
Nels Wadycki <nels.wadycki@gmail.com>
2017-06-04 13:49:06 -0500

    Went through Section 4, creating a book list application

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: 'Javascript', pages: 101 },
+        { title: 'Harry Potter', pages: 280 },
+        { title: 'The Dark Tower', pages: 200 },
+        { title: 'Futu.re', pages: 623 }
+    ]
+}
