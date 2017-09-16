44f2715b3fe9535404553381918a6bfcbc97bdba
Nels Wadycki <nels.wadycki@gmail.com>
2017-09-16 13:51:05 -0500

    Changes for the Weather App tutorial.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise';
 
-import SearchBar from './components/search_bar';
+import App from './components/app';
+import reducers from './reducers';
 
-const API_KEY = 'AIzaSyBRVyjh6xidwNDyrGW7pQP2OVmuDalvyWk';
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
-// Create a new component. This component should produce some HTML.
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
-}
-
-// Take this component's generated HTML and put it on the page (in the DOM)
-ReactDOM.render(<App />, document.querySelector('.container'));
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <App/>
+    </Provider>,
+    document.querySelector('.container')
+);
